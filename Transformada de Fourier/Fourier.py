5c35f5c0c8808d64776c6842d56b81c7d2be0e7d
Diego Martinez <df.martinezv@uniandes.edu.co>
2018-10-27 17:09:19 -0500

    Implementación de la transformada y gráfica

--- Transformada de Fourier/Fourier.py
+++ Transformada de Fourier/Fourier.py
@@ -3,15 +3,16 @@
 
 # ### Punto 2
 
-# In[1]:
+# In[3]:
 
 
 #Needed imports 
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.fft as fft
 
 
-# In[5]:
+# In[4]:
 
 
 #read the data from the dat archive signal
@@ -19,7 +20,7 @@ signal = np.loadtxt("signal.dat",delimiter = ",")
 incomplete = np.loadtxt("incompletos.dat",delimiter=",")
 
 
-# In[7]:
+# In[18]:
 
 
 #Separate the 2 columns of the data to make the plot 
@@ -30,5 +31,41 @@ plt.plot(signal_1,signal_2)
 plt.grid(True)
 plt.ylabel("Data in signal 1")
 plt.xlabel("Data in signal 0")
-plt.savefig("MartinezDiego_signal.pdf", type = "pdf")
+plt.savefig("MartinezDiego_signal.pdf", type = "PDF")
+
+
+# In[19]:
+
+
+#Implementation of fourier tranform
+def fourier(t):
+    n=len(t)
+    x=[]
+    for k in range(0,n): #the fourier transform as the sumatory of the ak coeff of the fourier series
+        sum=0
+        for i in range (0,n):
+            sum =  sum + t[i]*np.exp(-1j*2*np.pi*(k*i)/n)
+        x.append(sum)
+    return x
+
+
+# In[20]:
+
+
+#Fourier transform of the data and transformation of the freq
+Transf=fourier(signal_2)
+n = len(Transf)
+freq = fft.fftfreq(n)
+fixedfreq=2*np.pi*np.linspace(-n,n,n)/n
+t=np.linspace(0,n,n)
+
+
+# In[21]:
+
+
+plt.title("Grafica de la transformada")
+plt.plot(t,np.abs(Transf),label="Transformada")
+plt.legend(loc = 0)
+plt.grid(True)
+plt.savefig("MartinezDiego_TF.pdf", type = "PDF")
 
