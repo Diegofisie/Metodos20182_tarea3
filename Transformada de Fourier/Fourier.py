b709bfe2925bab75f4c31c7cb2cdde8b903d32c0
Diego Martinez <df.martinezv@uniandes.edu.co>
2018-10-28 11:22:51 -0500

    Fixed the fourier tranform
    Arrgelada la traansformada discreta de fourioer y las frecuencias con el método propio. Interpolacion de los datos incompletos

--- Transformada de Fourier/Fourier.py
+++ Transformada de Fourier/Fourier.py
@@ -3,16 +3,18 @@
 
 # ### Punto 2
 
-# In[3]:
+# In[26]:
 
 
 #Needed imports 
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.fft as fft
+from scipy.fftpack import ifft
+from scipy import interpolate
 
 
-# In[4]:
+# In[27]:
 
 
 #read the data from the dat archive signal
@@ -20,7 +22,7 @@ signal = np.loadtxt("signal.dat",delimiter = ",")
 incomplete = np.loadtxt("incompletos.dat",delimiter=",")
 
 
-# In[18]:
+# In[28]:
 
 
 #Separate the 2 columns of the data to make the plot 
@@ -34,38 +36,115 @@ plt.xlabel("Data in signal 0")
 plt.savefig("MartinezDiego_signal.pdf", type = "PDF")
 
 
-# In[19]:
+# In[29]:
 
 
 #Implementation of fourier tranform
-def fourier(t):
-    n=len(t)
-    x=[]
-    for k in range(0,n): #the fourier transform as the sumatory of the ak coeff of the fourier series
-        sum=0
-        for i in range (0,n):
-            sum =  sum + t[i]*np.exp(-1j*2*np.pi*(k*i)/n)
-        x.append(sum)
-    return x
+def fourier(signal_2):
+    N = len(signal_2)
+    transform = np.zeros([len(signal_2)],dtype=complex)
+    expo = np.exp(-2j*np.pi/N)
+    for n in range(N):
+        valorn = 0*np.exp(1j)
+        for k in range(N):
+            valorn+=(expo**(n*k))*signal_2[k]
+        transform [n]=valorn
+    return transform 
 
 
-# In[20]:
+# In[34]:
 
 
 #Fourier transform of the data and transformation of the freq
 Transf=fourier(signal_2)
 n = len(Transf)
-freq = fft.fftfreq(n)
-fixedfreq=2*np.pi*np.linspace(-n,n,n)/n
-t=np.linspace(0,n,n)
+pf = (1/(signal_1[1]-signal_1[0]))
+freq = np.concatenate((np.linspace(0,pf,len(signal_2))[0:256],np.linspace(-pf,0,len(signal_2))[255:511]))
+print("No se utilizo el paquete de fftfreq :)")
 
 
-# In[21]:
+# In[35]:
 
 
 plt.title("Grafica de la transformada")
-plt.plot(t,np.abs(Transf),label="Transformada")
+plt.plot(freq,np.abs(Transf),label="Transformada")
 plt.legend(loc = 0)
+plt.ylabel("Amplitud")
+plt.xlabel("Frecuencia[Hz]")
+plt.xlim([-1000,1000])
 plt.grid(True)
 plt.savefig("MartinezDiego_TF.pdf", type = "PDF")
+
+
+# In[36]:
+
+
+#lowpass filter
+fc=1000
+Transf[abs(freq)>fc]=0
+Tiempo=ifft(Transf)
+plt.figure()
+plt.grid(True)
+plt.plot(signal_1,np.real(Tiempo))
+plt.title("Datos filtrados")
+plt.ylabel("Senial")
+plt.xlabel("Tiempo")
+plt.savefig("MartinezDiego_filtrada.pdf",type = "pdf")
+
+
+# ### Datos incompletos
+
+# In[43]:
+
+
+incomp_1 = incomplete[:,0]
+incomp_2 = incomplete[:,1]
+plt.plot(imcomp_1,incomp_2)
+plt.title("Datos incompletos")
+plt.ylabel("Senal")
+plt.xlabel("Tiempo")
+plt.grid(True)
+print("No se puede encontrar la tranformada de los datos ya que estos tienen una tasa de muestreo muy pequena y esto hace que no hayan suficientes datos como para recontruir la ransformada")
+
+
+# In[49]:
+
+
+#Interpolaciones datos incompletos cuadratica y cubica
+fquadratic = interpolate.interp1d(incomp_1,incomp_2,kind = "quadratic")
+fcubic = interpolate.interp1d(incomp_1,incomp_2,kind = "cubic")
+data_x = np.linspace(imcomp_1[0],imcomp_1[-1],512)
+datafcuadra = fquadratic(data_X)
+datafcubic = fcubic(data_X)
+transform=fourier(incomp_2)
+tranformquadratic=fourier(datafcuadra)
+tranformcubic=fourier(datafcubic)
+pf = (1/(data_x[1]-data_X[0]))
+freqO = np.concatenate((np.linspace(0,pf,len(imcomp_1))[0:256],np.linspace(-pf,0,len(imcomp_1))[255:511]))
+freq = np.concatenate((np.linspace(0,pf,len(data_x))[0:256],np.linspace(-pf,0,len(data_X))[255:511]))
+
+
+# In[52]:
+
+
+plt.figure()
+plt.subplot(221)
+plt.plot(freqO,transform,"b",label="trans Original")
+plt.legend()
+plt.grid(True)
+plt.ylabel("Amplitud")
+plt.xlabel("Frecuencia[Hz]")
+plt.subplot(222)
+plt.plot(freq,tranformquadratic,"r",label="Quadratic" )
+plt.legend()
+plt.grid(True)
+plt.ylabel("Amplitud")
+plt.xlabel("Frecuencia[Hz]")
+plt.subplot(223)
+plt.plot(freq,tranformcubic,"y",label="Cubic")
+plt.legend()
+plt.grid(True)
+plt.ylabel("Amplitud")
+plt.xlabel("Frecuencia[Hz]")
+plt.savefig("MartinezDiego_TF_interpola.pdf",type = "pdf")
 
